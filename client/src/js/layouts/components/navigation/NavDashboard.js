4754a1a5e00e912bcd5ed36249d5830f9bee8af1
Sarap13 <saraparra13@gmail.com>
2024-04-10 15:28:38 +0100

    navbar changes

--- client/src/js/layouts/components/navigation/NavDashboard.js
+++ client/src/js/layouts/components/navigation/NavDashboard.js
@@ -3,11 +3,13 @@ import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGhost } from '@fortawesome/free-solid-svg-icons'
 import { Context } from "../../../store/appContext";
+import "./navbar.css"
+import easyopLogo from '../../../../img/easyopLogo.png'
 
 // Components
 
 export default function NavDashboard() {
-  const {store, actions} = useContext(Context)
+  const { store, actions } = useContext(Context)
   const navigate = useNavigate()
 
   const handleLogout = () => {
@@ -16,17 +18,17 @@ export default function NavDashboard() {
   }
 
   return (
-    <nav className="bg-white text-black sticky-top shadow">
+    <nav className="navbar-bg text-black sticky-top shadow">
       <div className="d-flex align-items-center container" style={{ height: "7vh" }}>
 
         <div className="me-auto fw-lighter">
           <Link to="/" className="text-decoration-none">
-            <FontAwesomeIcon icon={faGhost} className="me-2" style={{height:'35px'}}/><span className="fw-bold">Template</span>
+            <img src={easyopLogo} className="card-img-top mt-3" alt="..." style={{ width: "40%" }}></img>
           </Link>
         </div>
 
         <div>
-          <span className="mb-0 text-primary fw-normal text-decoration-underline" style={{cursor:'pointer'}} onClick={handleLogout}>Logout</span>
+          <span className="mb-0 text-primary fw-normal text-decoration-underline" style={{ cursor: 'pointer' }} onClick={handleLogout}>Logout</span>
         </div>
 
       </div>
--- client/src/js/layouts/components/navigation/Navbar.js
+++ client/src/js/layouts/components/navigation/Navbar.js
@@ -1,14 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGhost } from '@fortawesome/free-solid-svg-icons'
 import "./navbar.css"
 import easyopLogo from '../../../../img/easyopLogo.png'
 
 export default function Navbar() {
 
   return (
-    <nav className="bg-success text-black sticky-top shadow">
+    <nav className="navbar-bg text-black sticky-top">
       <div className="d-flex align-items-center container" style={{ height: "7vh" }}>
 
         <div className="me-auto fw-lighter">
@@ -20,15 +19,15 @@ export default function Navbar() {
         </div>
 
 
-          <div>
-            <Link to="/login" className="text-decoration-none">
-              <span className="mb-0 me-4">Login</span>
-            </Link>
-            <Link to="/signup/" className="text-decoration-none">
-              <button className="btn btn-sm btn-primary text-dark mb-0">New Account</button>
-            </Link>
-          </div>
-    
+        <div>
+          <Link to="/login" className="text-decoration-none">
+            <span className="mb-0 me-4">Login</span>
+          </Link>
+          <Link to="/signup/" className="text-decoration-none">
+            <button className="btn btn-sm btn-primary text-dark mb-0">New Account</button>
+          </Link>
+        </div>
+
 
       </div>
     </nav>
