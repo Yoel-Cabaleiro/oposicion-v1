95cd08e1880c19b56a5ab7ff0dc9bd10673aaa5a
Sarap13 <saraparra13@gmail.com>
2024-05-12 16:41:53 +0100

    WIP accordeon for extra info about questions

--- client/src/js/pages/questions/questions.js
+++ client/src/js/pages/questions/questions.js
@@ -48,7 +48,7 @@ export default function Questions() {
         }
     }
 
-    const resolve = async(index, item) => {
+    const resolve = async (index, item) => {
         const nuevasClases = [...clases]
         if (item.correct === true) {
             nuevasClases[index] = "success"
@@ -83,10 +83,10 @@ export default function Questions() {
     // Deberiamos hacer una ruta dinámica en router.js para que depende de que categoria de las que ha seleccionado el usuario se haga una ruta dependiendo del id para que se cojan las preguntas del endpoint.
 
     return (
-        <div className="container-fluid min-vh-100 overflow-hidden mt-3 mb-0 mx-auto">
-            <h2 className="m-5 mb-4 px-5 mx-3 text-start lato-regular fw-bold" >PRÁCTICA</h2>
+        <div className="container-fluid min-vh-100 overflow-hidden mt-3 mb-0 mx-auto ">
+            <h2 className="m-5 mb-4 px-5 mx-3 text-start lato-regular fw-bold text-black" >PRÁCTICA</h2>
             {pregunta && (<div className="container text-start mx-auto px-5 fs-6 text-justify">
-                <div className="mb-5"><h4 className="lato-regular">{Object.keys(pregunta)[0]}. {Object.values(pregunta)[0]}</h4></div>
+                <div className="mb-5"><h4 className="lato-regular text-black">{Object.keys(pregunta)[0]}. {Object.values(pregunta)[0]}</h4></div>
                 <ul>
                     {pregunta && pregunta.Respuestas.map((item, index) => {
                         return (
@@ -96,21 +96,30 @@ export default function Questions() {
                 </ul>
             </div>)}
             <hr className="m-5"></hr>
-            <div className="container-fluid">
-                <div className="row d-flex flex-row justify-content-between px-3">
-                    <div className="col-4 p-3 mx-5 bg-questions-media rounded d-flex flex-row justify-content-between">
-                        <p className="fw-bold">Preguntas acertadas</p>
-                        <p className="fw-bold">{aciertos} / {count}</p>
-                    </div>
-                    <div className="col-3">
-                        <button className="btn btn-outline border border-dark btn-lg align-self-end px-5" onClick={() => handleNext()}>Siguiente</button>
+            <div className="container-fluid d-flex flew-row mx-auto">
+                <div className="accordion accordion-flush d-flex flex-row rounded rounded-3 w-100 justify-content-center" id="accordionFlush">
+                    <div class="accordion-item col-6 ms-4 w-100">
+                        <h2 class="accordion-header" id="flush-headingOne">
+                            <button class="accordion-button collapsed lato-regular" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">Más info sobre tu práctica</button>
+                        </h2>
+                        <div className=" accordion-collapse collapse" id="flush-collapseOne" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlush">
+                            <div className="row d-flex flex-row px-2 accordion-body justify-content-between" >
+                                <div className="col-5 p-3 bg-questions-media rounded d-flex flex-row justify-content-between">
+                                    <p className="fw-bold">Preguntas acertadas</p>
+                                    <p className="fw-bold">{aciertos} / {count}</p>
+                                </div>
+                                <div className="col-5 p-3 ms-1 bg-questions-media rounded d-flex flex-row justify-content-between">
+                                    <p className="fw-bold">Porcentaje de preguntas acertadas</p>
+                                    <p className="fw-bold">{count != 0 ? ((aciertos / count) * 100).toFixed(1) + " %" : null}</p>
+                                </div>
+                            </div>
+                            <div className="row d-flex flex-row justify-content-between my-4 px-3">
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="row d-flex flex-row justify-content-between my-4 px-3">
-                    <div className="col-4 p-3 mx-5 bg-questions-media rounded d-flex flex-row justify-content-between">
-                        <p className="fw-bold">Porcentaje de preguntas acertadas</p>
-                        <p className="fw-bold">{count != 0 ? ((aciertos / count) * 100).toFixed(1) + " %" : null}</p>
-                    </div>
+                <div className="col-6 d-flex justify-content-center">
+                    <button className=" w-50 btn btn-outline-dark rounded rounded-3 fs-6 border border-dark btn-lg align-self-end px-5 lato-regular" onClick={() => handleNext()}>Siguiente</button>
                 </div>
             </div>
         </div>
