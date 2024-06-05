9142675f6d0e3e624b84b3dccb2a00c45add4bec
Sarap13 <saraparra13@gmail.com>
2024-06-05 23:59:52 +0100

    in return option from localstorage is located, missing create the handleclik function

--- client/src/js/pages/Stripe/return.js
+++ client/src/js/pages/Stripe/return.js
@@ -7,6 +7,25 @@ export function Return() {
     const [error, setError] = useState(null);
     const [subscription, setSubscription] = useState('');
 
+    const handleClick = (subscription) => {
+        //  PENDIENTE //
+        // subscription
+        // actions.setCategoriasSeleccionadas(finalCategories);
+        // Cuando le demos al boton queremos pasarle subscription para actualizar el fetch de put de estudiante y al abrir el seleccionar categorias hacer condicional si es oneCategory que el lenght sea 1 y si es el otro que sea mas grande.
+
+        // 1. coges subscription y lo actualizas en el modelo
+        // 2. en el boton handleClick llamamos al endpoint de put de estudiante y cambiamos la subscripcion
+
+        // EJEMPLO
+        // try: 
+        // AWAIT ACTION ACTUALIZAR ESTUDIANTE (SUSCRIPTION)
+        subscription
+        console.log(store.categoriasSeleccionadas)
+        navigate("/dashboard")
+
+    }
+
+
     useEffect(() => {
         const queryString = window.location.search;
         const urlParams = new URLSearchParams(queryString);
@@ -27,7 +46,14 @@ export function Return() {
                     } else {
                         setStatus(data.status);
                         setCustomerEmail(data.customer_email);
-                        setSubscription(data.subscription);
+                        // Cogemos los detalles del usuario del local storage
+                        const paymentDetails = localStorage.getItem("payment");
+                        // lo parseamos de json a js para poder usarlo
+                        const paymentData = JSON.parse(paymentDetails);
+                        console.log(paymentData.option)
+                        // seteamos subscripcion con la option del localstorage ya guardada en nuestro js.
+                        setSubscription(paymentData.option);
+
                     }
                 })
                 .catch((error) => {
@@ -38,11 +64,35 @@ export function Return() {
         }
     }, []);
 
+    useEffect(() => {
+        if (subscription) {
+            console.log('Subscription updated:', subscription);
+        }
+    }, [subscription]);
+
     if (error) {
-        return <div>Error: {error}</div>;
+        return console.log(error)
+        // <div>Error: {error}</div>;
     }
 
     if (status === "open") {
+        // <div id="pending" className="container pt-3 min-vh-100 d-flex justify-content-center align-items-center">
+        //     <div className='d-flex flex-column justify-content-center align-items-center bg-black text-white p-4 rounded rounded-3 opacity-75'>
+        //         <div className="text-center pt-5">
+        //             <i className="fa fa-circle-exclamation fa-6x m-1 text-danger"></i>
+        //             <h5 className="mt-5 lato-regular">Opps!</h5>
+        //             <span className="fs-1 lato-regular bolt">No se pudo completar tu pago. Por favor, revisa tus detalles de pago y vuelve a intentarlo.</span><br />
+        //             <button type="button" className="btn btn-light btn-lg px-5 mt-4 rounded rounded-3 fuente-lato fw-light">Reintentar pago</button>
+        //         </div>
+
+        //         <div className="mt-3 mb-5 text-center">
+        //             <h6 className="lato-regular mt-3">Ya puedes prácticar con nosotros con el plan {subscription}</h6>
+        //             <Link to="/selectCategory" className='text-decoration-none'>
+        //                 <button type="button" className="btn btn-light btn-lg px-5 mt-4 rounded rounded-3 fuente-lato fw-light">Clica aquí para seleccionar las categorías</button>
+        //             </Link>
+        //         </div>
+        //     </div>
+        // </div>
         return <Navigate to="/paymentdetails" />;
     }
 
@@ -53,20 +103,20 @@ export function Return() {
                     <div className="text-center pt-5">
                         <i className="fa fa-check-circle fa-6x m-1 text-success"></i>
                         <h5 className="mt-5 lato-regular">¡Pago recibido con éxito!</h5>
-                        <span className="fs-1 lato-regular bolt">Bienvenido a EasyOp,</span><br></br>
+                        <span className="fs-1 lato-regular bolt">Bienvenido a EasyOp,</span><br />
                         <span className="fs-1 lato-regular">{customerEmail}</span>
                     </div>
 
                     <div className="mt-3 mb-5 text-center">
-                        <h6 className="lato-regular mt-3">Ya puedes prácticar con nosotros con el plan</h6>
+                        <h6 className="lato-regular mt-3">Ya puedes prácticar con nosotros con el plan {subscription}</h6>
                         {/* <p>Suscripción: {subscription}</p> */}
                         <Link to="/selectCategory" className='text-decoration-none'>
-                            <button type="button" className="btn btn-light btn-lg px-5 mt-4 rounded rounded-3 fuente-lato fw-light">Clica aquí para seleccionar las categorías</button>
+                            <button type="button" className="btn btn-light btn-lg px-5 mt-4 rounded rounded-3 fuente-lato fw-light" onClick={handleClick}>Clica aquí para seleccionar las categorías</button>
                         </Link>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 
     return <div>Loading...</div>;
